4c23cac0b7ac13950b330cb992161e0b45e3aa01
Yuting <yutingli0606@gmail.com>
2025-02-18 23:06:16 +0800

    update baseline.sh

--- run_new.sh
+++ run_new.sh
@@ -4,6 +4,6 @@ do
     do
     #python main.py --model="der" --load_best_args --dataset="seq-tinyimg" --seed=$SEED --n_tasks=$N_TASKS --buffer_size=500 --run_name="der seed $SEED $N_TASKS tasks" --experiment_name="seq-tinyimg"
     #python main.py --model="derid" --load_best_args --dataset="seq-cifar100" --seed=$SEED --n_tasks=$N_TASKS --buffer_size=500 --run_name="derpp seed $SEED $N_TASKS tasks" --experiment_name="seq-cifar100"
-    python main.py --model="derloss" --load_best_args --dataset="seq-cifar10" --device="cuda:6" --seed=$SEED --n_tasks=$N_TASKS --buffer_size=500 --run_name="derlossplot seed $SEED $N_TASKS tasks" --experiment_name="seq-cifar10"
+    python main.py --model="derloss" --load_best_args --dataset="seq-cifar10" --device="cuda:5" --seed=$SEED --n_tasks=$N_TASKS --buffer_size=500 --run_name="derlossplot seed $SEED $N_TASKS tasks" --experiment_name="seq-cifar10"
     done
 done
